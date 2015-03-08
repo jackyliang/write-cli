6bbeb77bf9da425d180925b0ed0582cd598394a8
Jacky Liang <bluelegopiece@hotmail.com>
2015-03-08 16:25:52 +0800

    added write_cli.sh script

---
+++ write-cli.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+### Begin User Configs
+
+# Please set your default editor and save path
+alias EDITOR="DYLD_FORCE_FLAT_NAMESPACE=1 ~/Applications/MacVim.app/Contents/MacOS/Vim"
+# Set the save path 
+SAVE_PATH="$HOME/Desktop/" 
+
+### End User Configs
+
+# Date used for the file name
+NOW_FILE=$(date +"%B_%d_%Y") 
+
+# Date used within the entry
+NOW_STRING=$(date +"%B %d %Y %r") 
+
+# Output the date to the file 
+echo $NOW_STRING > $SAVE_PATH$NOW_FILE.txt 
+
+# Open the text editor
+EDITOR $SAVE_PATH$NOW_FILE.txt
