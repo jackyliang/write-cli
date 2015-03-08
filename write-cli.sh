66c6e0dfcef665da66608798ecaa9f2db62944f4
Jacky Liang <bluelegopiece@hotmail.com>
2015-03-08 17:09:44 +0800

    updated date format and docs

--- write-cli.sh
+++ write-cli.sh
@@ -9,13 +9,13 @@ SAVE_PATH="$HOME/Desktop/Write/"
 
 ### End User Configs
 
-# Date used for the file name
+# Date used for the file name i.e. July_13_1993.txt
 NOW_FILE=$(date +"%B_%d_%Y") 
 
-# Date used within the entry
-NOW_STRING=$(date +"%B %d %Y %r") 
+# Date used within the entry i.e. July 13 1993 1:00 PM Sunday
+NOW_STRING=$(date +"%B%e %Y%l:%M %p %A") 
 
-# Append the date to the file 
+# Append the date to the file so previous entries are not overridden 
 echo "$NOW_STRING" >> "$SAVE_PATH$NOW_FILE.txt" 
 
 # Open the text editor
